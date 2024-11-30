14f8993fcf2f548aeca002d688e185f89cf52863
Aditya Bhave <102917051+adityabhave525@users.noreply.github.com>
2024-11-30 19:23:13 +0530

    TipTap Editor Init

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,3 +1,5 @@
+import { Editor } from "./editor";
+
 interface DocumentIdPageProps {
     params: Promise<{ documentId: string}>;
 };
@@ -7,8 +9,10 @@ const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
     const { documentId } = await params;
 
   return (
-    <div>Document ID: {documentId}</div>
+    <div className="min-h-screen bg-[#FAFBFD]">
+      <Editor />  
+    </div>
   )
 }
 
-export default DocumentIdPage
+export default DocumentIdPage;
