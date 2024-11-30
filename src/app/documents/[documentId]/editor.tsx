bec355482c137bf0674185a6fc44fd0d40f94676
Aditya Bhave <102917051+adityabhave525@users.noreply.github.com>
2024-12-01 00:09:08 +0530

    Toolbar Init

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -11,9 +11,37 @@ import Image from '@tiptap/extension-image';
 import ImageResize from 'tiptap-extension-resize-image';
 import { useEditor, EditorContent } from '@tiptap/react';
 
+import { useEditorStore } from '@/store/use-editor-store';
+
 export const Editor = () => {
 
+    const { setEditor } = useEditorStore();
+
     const editor = useEditor({
+        onCreate({ editor }) {
+            setEditor(editor);
+        },
+        onDestroy() {
+            setEditor(null);
+        },
+        onUpdate({ editor }) {
+            setEditor(editor);
+        },
+        onSelectionUpdate({ editor }) {
+            setEditor(editor);
+        },
+        onTransaction({ editor }) {
+            setEditor(editor);
+        },
+        onFocus({ editor }) {
+            setEditor(editor);
+        },
+        onBlur({ editor }) {
+            setEditor(editor);
+        },
+        onContentError({ editor }) {
+            setEditor(editor);
+        },
         editorProps: {
             attributes: {
                 style: "padding-left: 56px; padding-right: 56px;",
