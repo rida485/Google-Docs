72b2525facec52a49d81f15392d933901ba99bc3
Aditya Bhave <102917051+adityabhave525@users.noreply.github.com>
2024-12-11 00:37:52 +0530

    Clerk Setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    providers: [
+        {
+            domain: "https://handy-manatee-67.clerk.accounts.dev",
+            applicationID: "convex",
+        }
+    ]
+}
